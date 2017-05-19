17cbb74d6f9f2fb8863025ccc3ff0c51f6087c48
akrupych <akrupych@remit.se>
2017-05-19 13:49:45 +0300

    kotlin support

---
+++ app/src/main/java/se/remit/akrupych/magindevtestapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package se.remit.akrupych.magindevtestapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        helloWorld.text = "Hello Kotlin!"
+    }
+}
